e906572aea19744ebc8cb170e83956010355ef0e
mh3mh0 <jonxca@icloud.com>
2023-03-21 10:30:53 +0100

    added class 03

---
+++ 03_strings.py
@@ -0,0 +1,18 @@
+'''
+strings ------------------->
+'''
+my_string = 'my string'
+my_other_string = 'my other string'
+
+print(len(my_string))
+print(len(my_other_string))
+print(my_string + ' SUP? ' + my_other_string)
+
+my_new_line_string = 'this is an string\nwith a line break' # line break
+print(my_new_line_string)
+
+my_tab_string = '\tthis is an string with a line tab' # line tab
+print(my_tab_string)
+
+my_scape_string = '\tthis is an string with a \n line scape' # line tab
+print(my_scape_string)
