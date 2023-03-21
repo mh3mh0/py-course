91c26388fed50ee7318b2203546310f0c81e3954
mh3mh0 <jonxca@icloud.com>
2023-03-21 09:32:11 +0100

    updated clase 02

--- 02_operators.py
+++ 02_operators.py
@@ -1,7 +1,6 @@
 '''
  arithmetical operators ------------------->
 '''
-
 print (3 + 4)
 print (3 - 4)
 print (3 * 4)
@@ -18,6 +17,8 @@ print('hi there, ' * (2 ** 3))
 
 my_float = 2.5 * 2
 print('hi there, ' *int(my_float))
+
+
 
 '''
  comparative operators ------------------->
@@ -38,5 +39,17 @@ print('hi there' == 'Python')
 print('hi there' != 'Python')
 print('hi there' >= 'SUP') # alphabetical ordering
 print(len('hi there') <= len('SUP')) # count characters
+
+
+
+'''
+logical operators ------------------->
+'''
+print(3 > 4 and 'hi' > 'Python') # &&
+print(3 > 4 or 'hi' > 'Python') # ||
+print(3 < 4 and 'hi' < 'Python') # &&
+print(3 < 4 or 'hi' > 'Python') # ||
+print(3 > 4 or 'hi' > 'Python') # ||
+print(not(3 > 4)) # (!=)
 
 
