352f2a70fdb7b8d2d35e9e1344b5e3f6168ddbea
mh3mh0 <jonxca@icloud.com>
2023-03-22 17:28:34 +0100

    added class 04

--- 03_strings.py
+++ 03_strings.py
@@ -16,3 +16,57 @@ print(my_tab_string)
 
 my_scape_string = '\tthis is an string with a \n line scape' # line tab
 print(my_scape_string)
+
+'''
+formatting ------------------->
+'''
+name, surname, age = 'John', 'Ruiz', 41
+
+print('My name is {} {} and my age is {}'.format(name, surname, age)) # correct
+print('My name is %s %s and my age is %d' %(name, surname, age)) # correct
+print('My name is ' + name + ' ' + surname + ' and my age is ' + str(age)) # incorrect
+print(f'My name is {name} {surname} and my age is {age}') # the best option
+
+
+'''
+un-packing characters ------------------->
+'''
+language = 'python'
+a, b, c, d, e, f = language
+print(a)
+print(f)
+
+
+'''
+Division ------------------->
+'''
+language_slice = language[1:3]
+print(language_slice)
+
+language_slice = language[1:]
+print(language_slice)
+
+language_slice = language[0:6:2]
+print(language_slice)
+
+
+'''
+Reverse ------------------->
+'''
+reversed_language = language[::-1]
+print(reversed_language)
+
+
+'''
+Functions ------------------->
+'''
+
+print(language.capitalize())
+print(language.upper())
+print(language.count('t'))
+print(language.isnumeric())
+print('1'.isnumeric())
+print(language.lower())
+print(language.lower().isupper())
+print(language.startswith('py'))
+print('Py' == 'py') # it is not the same
---
+++ 04_lists.py
@@ -0,0 +1,27 @@
+'''
+lists ------------------->
+'''
+
+my_list = list()
+my_other_list = []
+
+print(len(my_list))
+
+my_list = [335, 24, 62, 52, 30, 30, 17]
+
+print(my_list)
+print(len(my_list))
+
+my_other_list = [35, 1.77, 'john', 'ruiz']
+
+print(type(my_list))
+print(type(my_other_list))
+
+print(my_other_list[0])
+print(my_other_list[1])
+print(my_other_list[-1])
+print(my_other_list[-3])
+print(my_other_list[-4])
+print(my_list.count(30))
+# print(my_other_list[4]) IndexError
+# print(my_other_list[-5]) IndexError
